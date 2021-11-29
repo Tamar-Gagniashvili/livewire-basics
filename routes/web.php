e9bd2719f6045f4d7aa20a17502e9053a2028f63
Tamar Gagniashvili <tamo.gagniashvili@gmail.com>
2021-11-29 21:38:14 +0400

    feat: add contact page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Mail\ContactFormMailable;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('examples');
+});
+
+Route::post('/contact', function (Request $request) {
+    $contact = $request->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+        'phone' => 'required',
+        'message' => 'required',
+    ]);
+
+    Mail::to('andre@andre.com')->send(new ContactFormMailable($contact));
+
+    return back()->with('success_message', 'We received your message successfully and will get back to you shortly!');
 });
